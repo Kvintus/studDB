e18a31c87f00c57ce57c951bd93c40f6213b60d3
Jakob Rolik <daidaloskvintus@gmail.com>
2017-11-09 14:12:31 +0100

    vela

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 from flask import Flask, flash, redirect, render_template, request, session, url_for
 from cs50 import SQL
-
+from helpers.sqlClasses import *
 
 app = Flask(__name__)
-db = SQL("sqlite:///assets/databaza.db")
+
 
 @app.route('/')
 def index():
-    return 'Hi'
+    s = Students.query().all()
+    return render_template('main_template.html' , ala = s)
 
 app.run(host='0.0.0.0', port='8080')
